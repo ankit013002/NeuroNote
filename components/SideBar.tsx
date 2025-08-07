2b1da623af2cc4e4bf5a13be3faefdcf78cd5c9a
ankit013002 <112412889+ankit013002@users.noreply.github.com>
2025-08-06 21:15:14 -0400

    Added to the sidebar by adding a sheet.

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { Button } from "./ui/button";
 
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { MenuIcon } from "lucide-react";
+
 const SideBar = () => {
   return (
     <div className="flex flex-col gap-y-2">
-      <Button>New Document</Button>
+      {/* 
       <div className="text-gray-600 font-bold text-[0.75em]">My Documents</div>
       {Array.from({ length: 10 }).map((_, index) => (
         <button
@@ -13,7 +23,26 @@ const SideBar = () => {
         >
           Document
         </button>
-      ))}
+      ))} */}
+      <div className="hidden md:inline">
+        <Button>New Document</Button>
+      </div>
+      <Sheet>
+        <SheetTrigger>
+          <MenuIcon
+            className="inline md:hidden p-2 hover:opacity-50"
+            size={45}
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className="items-center">
+          <SheetHeader>
+            <SheetTitle className="text-center">Menu</SheetTitle>
+            <div>
+              <Button>New Document</Button>
+            </div>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
     </div>
   );
 };
